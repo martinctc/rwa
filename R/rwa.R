e8ccd511f75c4abeaca70f88fd48542089243061
Martin Chan <martinchan53@gmail.com>
2021-02-04 22:04:04 +0000

    feat: add bootstrap CI
    work in progress code - does not work yet

--- R/rwa.R
+++ R/rwa.R
@@ -115,4 +115,106 @@ rwa <- function(df,
        "lambda" = lambda,
        "RXX" = RXX,
        "RXY" = RXY)
+
+  # ## Bootstrap results TBD
+  # #Bootstrapped Confidence interval around the individual relative weights
+  # #Please be patient -- This can take a few minutes to run
+  # multBoot<-boot(thedata, multBootstrap, 10000)
+  # multci<-boot.ci(multBoot,conf=0.95, type="bca")
+  # runBoot(length(thedata[,2:numVar]))
+  # CI.Results<-CIresult
+  #
+  # #Bootstrapped Confidence interval tests of Significance
+  # #Please be patient -- This can take a few minutes to run
+  # randVar<-rnorm(length(thedata[,1]),0,1)
+  # randData<-cbind(thedata,randVar)
+  # multRBoot<-boot(randData,multBootrand, 10000)
+  # multRci<-boot.ci(multRBoot,conf=0.95, type="bca")
+  # runRBoot(length(randData[,2:(numVar-1)]))
+  # CI.Significance<-CIresult
+  #
+  #
+  # #R-squared For the Model
+  # RSQ.Results
+  #
+  #
+  # #The Raw and Rescaled Weights
+  # RW.Results
+  # #BCa Confidence Intervals around the raw weights
+  # CI.Results
+  # #BCa Confidence Interval Tests of significance
+  # #If Zero is not included, Weight is Significant
+  # CI.Significance
+}
+
+#' @title Multiple Regression Bootstrap
+#'
+#' @description Internal function for multiple regression rwa
+
+multBootstrap <-function(mydata, indices){
+  mydata <- mydata[indices,]
+  multWeights <- multRegress(mydata)
+  multWeights$Raw.RelWeight
+}
+
+multBootrand<-function(mydata, indices){
+  mydata <- mydata[indices,]
+  multRWeights <- multRegress(mydata)
+  multReps <- multRWeights$Raw.RelWeight
+  randWeight <- multReps[length(multReps)]
+  randStat <- multReps[-(length(multReps))] - randWeight
+  randStat
+}
+
+#' @title Bootstrap CI
+mybootci <- function(x){
+  boot::boot.ci(multBoot,
+                conf=0.95,
+                type="bca",
+                index=x)
+}
+
+runBoot <- function(num){
+  INDEX <- 1:num
+  test <- lapply(INDEX, FUN=mybootci)
+  test2 <- t(sapply(test,'[[',i=4)) # Extracts confidence interval
+  CIresult <<- data.frame(Variables,
+                          CI.Lower.Bound=test2[,4],
+                          CI.Upper.Bound=test2[,5])
+}
+
+myRbootci <- function(x){
+  boot::boot.ci(multRBoot,
+                conf=0.95,
+                type="bca",
+                index=x)
+}
+
+runRBoot<-function(num){
+  INDEX<-1:num
+  test<-lapply(INDEX,FUN=myRbootci)
+  test2<-t(sapply(test,'[[',i=4))
+  CIresult<<-data.frame(Labels,CI.Lower.Bound=test2[,4],CI.Upper.Bound=test2[,5])
+}
+
+myCbootci<-function(x){
+  boot.ci(multC2Boot,conf=0.95,type="bca",index=x)
+}
+
+runCBoot<-function(num){
+  INDEX<-1:num
+  test<-lapply(INDEX,FUN=myCbootci)
+  test2<-t(sapply(test,'[[',i=4))
+  CIresult<<-data.frame(Labels2,CI.Lower.Bound=test2[,4],CI.Upper.Bound=test2[,5])
+}
+
+myGbootci<-function(x){
+  boot.ci(groupBoot,conf=0.95,type="bca",index=x)
+}
+
+runGBoot<-function(num){
+  INDEX<-1:num
+  test<-lapply(INDEX,FUN=myGbootci)
+  test2<-t(sapply(test,'[[',i=4))
+  CIresult<<-data.frame(Labels,CI.Lower.Bound=test2[,4],CI.Upper.Bound=test2[,5])
 }
